61241759adf6c29e2d3010a56ca65367a3da6bc2
RVF400 <41142900+RVF400@users.noreply.github.com>
2022-07-12 17:20:34 +0100

    Added sounds

--- main.ts
+++ main.ts
@@ -1,8 +1,12 @@
 input.onButtonPressed(Button.A, function () {
+    music.playMelody("C5 A C5 A C5 A C5 A ", 120)
     while (!(input.buttonIsPressed(Button.B))) {
         pins.digitalWritePin(DigitalPin.P0, 1)
         basic.pause(500)
         pins.digitalWritePin(DigitalPin.P0, 0)
         basic.pause(500)
     }
+})
+input.onButtonPressed(Button.B, function () {
+    music.stopMelody(MelodyStopOptions.All)
 })
